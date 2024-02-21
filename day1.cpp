9c3d5ada1004bf11820869591f1cd347a5de2329
Ritwija <ritwija05@gmail.com>
2024-02-21 23:46:49 +0530

    d

---
+++ day1.cpp
@@ -0,0 +1,24 @@
+int findEquilibrium(int A[], int n)
+{
+  
+    if(n == 0)
+        return 0;
+        
+    int sum, left;
+    sum = 0;
+    left = 0;
+    
+    for(int i=0; i<n; i++)
+        sum += A[i];
+        
+    for(int i=0; i<n; i++){
+        sum -= A[i];
+        
+        if(sum == left)
+            return i;
+            
+        left += A[i];
+    }
+    
+    return -1;
+}
