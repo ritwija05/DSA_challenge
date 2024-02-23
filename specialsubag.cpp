131e3c200b4e1e2012c23f15f2a9ce68f964af15
Ritwija <ritwija05@gmail.com>
2024-02-23 23:27:00 +0530

    f

---
+++ specialsubag.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+#include<string>
+
+using namespace std;
+int solve(string A) {
+    int count = 0;
+    int ans = 0;
+    for (int i = A.size() - 1; i >= 0; i--) {
+        if (A[i] == 'G') 
+        {
+            count++;
+        } else if (A[i] == 'A') {
+            ans += count;
+        }
+    }
+    return count;
+}
